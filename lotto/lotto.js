05c9eab069c3cc813372b23246a38c09f61c538a
jinioh <93959611+jinioh@users.noreply.github.com>
2024-01-19 12:02:45 +0900

    lotto

---
+++ lotto/lotto.js
@@ -0,0 +1,30 @@
+const candidate = Array(45).fill().map((v, i) => i + 1);
+const shuffle = [];
+while (candidate.length > 0) {
+    const random = Math.floor(Math.random() * candidate.length);
+    const spliceArray = candidate.splice(random, 1);
+    const value = spliceArray[0];
+    shuffle.push(value);
+}
+
+const winBalls = shuffle.slice(0,6).sort((a, b) => a - b);
+const bonus = shuffle[6];
+const $result = document.querySelector('#result');
+function drawBall(number, $parent) {
+    const $ball = document.createElement('div');
+    $ball.className = 'ball';
+    $ball.textContent = number;
+    $parent.append($ball);
+}
+
+for (let i = 0; i < winBalls.length; i++) {
+    setTimeout(() => {
+        drawBall(winBalls[i], $result);
+    }, 1000 * (i + 1));
+}
+
+
+const $bonus = document.querySelector('#bonus');
+setTimeout(() => {
+    drawBall(bonus, $bonus);
+}, 7000);
